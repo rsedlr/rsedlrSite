1d2259bb27971122e8228bfe03efe6373e74a436
undefined <reissedler@gmail.com>
2018-12-11 20:54:14 +0000

    a

--- isItChristmasYet.py
+++ isItChristmasYet.py
@@ -1,8 +1,9 @@
 timer = False
-while timer == False:
-  inpt = str(input("Enter \'deploy\' to go live\n> "))
-  if inpt.upper() == 'DEPLOY':
-    print('whoop whoop we are go for lift off!!!!!!!!!!!!')
-    timer = True
-  else:
-    print('\ninvalid command...\n')
+if __name__ == "__main__":  
+  while timer == False:
+    inpt = str(input("Enter \'deploy\' to go live\n> "))
+    if inpt.upper() == 'DEPLOY':
+      print('whoop whoop we are go for lift off!!!!!!!!!!!!')
+      timer = True
+    else:
+      print('\ninvalid command...\n')
