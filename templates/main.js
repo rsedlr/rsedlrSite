7c7a494e673383d242453baa7f79a6c76255f37d
undefined <reissedler@gmail.com>
2018-12-15 01:19:09 +0000

    a

--- templates/main.js
+++ templates/main.js
@@ -50,6 +50,6 @@ $(document).ready(function () {
       });
     });
   }
-  window.fitText( document.getElementById("mainText"), 1 ); 
-  window.fitText( document.getElementById("secText"), 1 ); 
+  window.fitText( document.getElementById("mainText"), 0.7 ); 
+  window.fitText( document.getElementById("secText"), 1.1 ); 
 });
