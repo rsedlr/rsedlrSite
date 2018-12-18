e8dbd8449ef4e803094b221d42fd68e8d0eb7ebd
MegaHertz3 <30870094+MegaHertz3@users.noreply.github.com>
2018-12-18 17:12:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -189,102 +189,22 @@ def christmas(name=''):
   else:
     message = messageTpl % ((name + ',') if name != '' else '', '<br><br><br>', 'From Reiss')
 
-  # nowTime = datetime.now()
-  # christmasTime = datetime(2018, 12, 25, 0)
-  # timeDiff = nowTime - christmasTime
-  # if timeDiff.total_seconds() >= 0:
-  #   return template('christmasCard-M', name=name, message=message)
-  # return template('notChristmas', name=name)
-
-  return template('christmasCard-M', name=name, message=message)
+  nowTime = datetime.now()
+  christmasTime = datetime(2018, 12, 25, 0)
+  timeDiff = nowTime - christmasTime
+  if timeDiff.total_seconds() >= 0:
+    return template('christmasCard-M', name=name, message=message)
+  return template('notChristmas', name=name)
 
 
 @route('/shhhnoonecanknowiusethis')
-@route('/shhhnoonecanknowiusethis/')
-@route('/shhhnoonecanknowiusethis/<name>')
-def fakeChristmas(name=''):
-  nameUp = name.upper()
-  name = name.replace('_', ' ')
-  messageTpl = '''
-      %s<br><br>
-      <br>     
-      Have a merry christmas,<br>
-      And a happy new year!<br>
-      %s
-      <br><br><br>
-      %s
-      '''
-  if nameUp == 'MUM':
-    message = '''
-      To Mum,<br><br>
-      <br>     
-      Wishing you a brilliant christmas,<br>
-      while saving the world by not using paper :)<br>
-      <br>
-      Hang in there, you can eat again soon!<br>
-      <br>
-      Lots of love Reiss xxx
-      ''' 
-  elif nameUp == 'DAD':
-    message = '''
-      To Dad,<br><br> 
-      <br>     
-      Not like this was much of a surprise for you :) <br>
-      Have a great christmas,<br>
-      And a brilliant new year!<br>
-      <br>
-      Can't wait to get back out on the bikes with you :)<br>
-      <br>
-      lots of love from Reiss
-      ''' 
-  elif nameUp == 'EVEY':
-    message = messageTpl %('To stinkie-wee-wee,', '<br>Pink is your fav colour right? <br>', 'Love from Reiss')
-  elif nameUp == 'JOEY':
-    message = messageTpl %('To Jobby,', '<br>At least you have a good excuse for being a sloth today :)', 'Love from Jobby Junior')
-  elif nameUp == 'NAN_A':
-    message = messageTpl %('To Nanny Anne,', '<br>Hope you got all the indoor slippers you wanted :)<br>', 'Lots of love from Reiss')
-  elif nameUp == 'GRANDAD_B':
-    message = messageTpl %('To the best grandad that ever lived,', '<br>Miss you more and more each day :(<br>hope you have a great christmas wherever you are<br>', 'Lots of love from Reiss')
-  elif nameUp == 'GRANDAD':
-    message = messageTpl %('To grandad,', '<br>Cant wait to see you tomorrow :)<br>', 'Lots of love from Reiss')
-  elif nameUp == 'TASH':
-    message = messageTpl %('To Tash,', '<br>Good luck with your final year at uni, i\'m sure you\'ll smash it!', 'Lots of love from Reiss')
-  elif nameUp == 'PIXIE':
-    message = 'Woof ' * 100 
-  elif nameUp == 'EVIE':
-    message = messageTpl %('To Evie,', '<br>I heard blue was your new favorite colour :)<br>', 'Love from Reiss')
-  elif nameUp == 'NAN_M':
-    message = messageTpl %('To Nanny M,', '<br>At least you get a chance to sit down this christmas :)<br>', 'Lots of love from Reiss')
-  elif nameUp == 'JEL':
-    message = messageTpl %('To Jel,', '<br>This one should fit in yer bag nice and easy :)<br>', 'Lots of love from Reiss')
-  elif nameUp == 'MR_MULLA':
-    message = messageTpl %('To Mr Mulla,', '<br>(Thought it would be easier to code a card than write a laod out by hand)', 'From Reiss')      
-  elif nameUp == 'MS_AZIZ':
-    message = messageTpl %('To Ms. Aziz,', '<br><br>', 'From Reiss<br>(y12 computing class)')      
-  elif nameUp == 'FRED':
-    message = messageTpl %('To Freddo,', '<br>you shagged a bird yet by any chance?<br>', 'From Reiss')
-  elif nameUp == 'BRETT':
-    message = messageTpl %('To Tiddles,', '<br>hope its good up that mountain<br>', 'From Reiss')      
-  elif nameUp == 'NICOLE':
-    message = messageTpl %('To Nicole,', '<br>i really dont hate you at all btw:)<br>', 'From Reiss') 
-  elif nameUp == 'REICE':
-    message = messageTpl %('To Reice,', '<br>Get well soon mate<br>', 'From Reiss')      
-  elif nameUp == 'IZZY':
-    message = messageTpl %('To Dizzy Fizzy Izzy,', '<br>Tell pandi I miss her<br>', 'From Kawala **Reiss (sorry)')      
-  elif nameUp == 'WILL':
-    name = 'Bill'
-    message = messageTpl %('sorry *Will,', '<br>we need to make a project one day soon :)<br>', 'From Reiss')
-  elif nameUp == 'JOE':
-    message = messageTpl %('Joe,', '<br>Ya soppy git<br>', 'From Reiss')
-  else:
-    message = messageTpl % ((name + ',') if name != '' else '', '', 'From Reiss')
-
-  return template('christmasCard-M', name=name, message=message)
-
-
-@route('/wallpaper')
-def wallpaper():
+def shhh(name=''):
   return template('wrappingPaper')
+
+
+# @route('/wallpaper')
+# def wallpaper():
+#   return template('wrappingPaper')
 
 
 if __name__ == '__main__':
