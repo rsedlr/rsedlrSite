a51e8a5f61a2cfd12c92505f3fb20c5f09d14ffa
MegaHertz3 <30870094+MegaHertz3@users.noreply.github.com>
2018-12-18 22:02:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -227,7 +227,7 @@ def christmas(name=''):
 @route('/shhhnoonecanknowiusethis')
 def shhh(name=''):
   nowTime = datetime.now()
-  christmasTime = datetime(2018, 12, 18, 0)
+  christmasTime = datetime(2018, 12, 18, 11)
   timeDiff = nowTime - christmasTime
   if timeDiff.total_seconds() >= 0:
     return '<p>Its christmas<p>'
