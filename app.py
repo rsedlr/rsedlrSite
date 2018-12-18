225c009be710de52a34746daf2712a629e66c9ad
MegaHertz3 <30870094+MegaHertz3@users.noreply.github.com>
2018-12-18 21:15:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ def christmas(name=''):
   if timeDiff.total_seconds() >= 0:
     return template('christmasCard-M', name=name, message=message)
 
-  prefix = ['MR ', 'MS ', 'MRS']
+  prefix = ['MR_', 'MS_', 'MRS']
   if nameUp[:3] not in prefix:
     name = name.split(' ')[0]
     print(nameUp[:3])
