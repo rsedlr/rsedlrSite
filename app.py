a09b83a559bcbc8a08aa6576008ab3d4a6cee857
MegaHertz3 <30870094+MegaHertz3@users.noreply.github.com>
2018-12-19 23:18:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ def shhh():
 def gitPull():
   git.cmd.Git('/var/www/rsedlr.xyz').pull()
   # subprocess.call("sudo git pull", shell=True)
-  print('doneeee pull')
+  print('doneeee pulllll')
 
 # @route('/wallpaper')
 # def wallpaper():
