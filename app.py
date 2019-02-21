bae9a4573f572777d97b555948041623b5ae45d7
MegaHertz3 <30870094+MegaHertz3@users.noreply.github.com>
2019-02-21 21:56:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ if __name__ == '__main__':
   # run_decoupled(app, '0.0.0.0', 80)
   run(host='0.0.0.0', port=80, server='cherrypy', reloader=True, debug=True)  # 127.0.0.1
 
-# wordsss
+# wordssssss
 
 '''
 
