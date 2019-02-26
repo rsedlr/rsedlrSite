2702b0083a7c155819d31efef206d89b8a347154
undefined <reissedler@gmail.com>
2019-02-26 21:43:25 +0000

    git reset orogin master

--- app.py
+++ app.py
@@ -236,6 +236,7 @@ def dadsGift():
 def gitPull():
   # git.cmd.Git('/var/www/rsedlrSite').pull()
   git.cmd.Git('/var/www/rsedlrSite').fetch()
+  os.write('sudo git reset --hard origin/master')
   print('\n************ Git pull done ************\n')
   # print('\n************ git update available ************\n')
 
