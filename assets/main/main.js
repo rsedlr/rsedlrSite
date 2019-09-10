307e98fe82a20a25b3673d37e8faa84bee0173ec
rs <reissedler@gmail.com>
2019-09-10 18:09:13 +0100

    improved scroll offset

--- assets/main/main.js
+++ assets/main/main.js
@@ -150,9 +150,9 @@ $(document).ready(function () {
   $("#contactForm").submit(function(event) {
     event.preventDefault();
     submitForm();
-  });            
+  });    
 
-  function submitForm(){
+  function submitForm() {
     var submitMsg = $("#msgSubmit");
     if (contactClicked) {
       submitMsg.text('Thanks, message submitted - If you would like to submit another message, please refresh the page');
@@ -179,3 +179,24 @@ $(document).ready(function () {
     }
   }
 });            
+
+// ! function (e) {
+//   "use strict";
+//   e('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+//     if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+//       var a = e(this.hash);
+//       if ((a = a.length ? a : e("[name=" + this.hash.slice(1) + "]")).length) return e("html, body").animate({
+//         scrollTop: a.offset().top - 48
+//       }, 1e3, "easeInOutExpo"), !1
+//     }
+//   }), e(".js-scroll-trigger").click(function () {
+//     e(".navbar-collapse").collapse("hide")
+//   }), e("body").scrollspy({
+//     target: "#mainNav",
+//     offset: 54
+//   });
+//   // var a = function () {
+//   //   100 < e("#mainNav").offset().top ? e("#mainNav").addClass("navbar-shrink") : e("#mainNav").removeClass("navbar-shrink")
+//   // };
+//   a(), e(window).scroll(a)
+// }(jQuery);
--- views/main.tpl
+++ views/main.tpl
@@ -157,7 +157,7 @@
     <!-- <script src="/static/mainPage/jquery.min.js"></script> -->
     <script src="/static/mainPage/bootstrap.bundle.min.js"></script>
     <script src="/static/mainPage/jquery.easing.min.js"></script>
-    <script src="/static/mainPage/main.min.js"></script>
+    <script src="/static/mainPage/scroll.js"></script>
   </body>
 </html>
 
