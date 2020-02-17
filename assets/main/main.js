0be9e33169580084b34300b082aa238cb0e06641
rsedlr <30870094+rsedlr@users.noreply.github.com>
2020-02-17 00:31:00 +0000

    added plant waterer image

--- assets/main/main.js
+++ assets/main/main.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
   
   var imgCount = {'heatDemo': 1, 'othello': 2,'wikiRace': 1, 'christmasCard': 2, 
                   'thisSite': 1, 'unity': 1, 'rcCar': 1, 'bl': 1, 
-                  'plantWaterer': 1, 'pyGame': 1, 'tempImg': 1}
+                  'plantWaterer': 2, 'pyGame': 1, 'tempImg': 1}
 
   if (isMac) {
     mainText.className += ' Mac-main';
