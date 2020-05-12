4fdd7c576dea7e6872815195b0f49c0948a4807d
Reiss <30870094+rsedlr@users.noreply.github.com>
2020-05-12 16:00:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ try:
 except Exception as e:
   print(e)
 
-try:
+try:  # new shit
   from cheroot.wsgi import Server as WSGIServer
 except ImportError:
   from cherrypy.wsgiserver import CherryPyWSGIServer as WSGIServer
