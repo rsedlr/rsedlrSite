33eb44ce171daf5229018bb44c99406af13fa82a
Reiss <30870094+rsedlr@users.noreply.github.com>
2020-10-03 13:50:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,8 @@ if not dev:
   @hook('after_request')
   def enable_cors():
     client_ip = request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR')
-    # stat = (" statCode(" + str(response.status_code) + ")") if str(response.status_code) != "200" else ""
-    logging.debug("IP(" + client_ip + ") method(" + request.method + ") path(" + request.path + ") status(" + request.status_code + ")")
+    stat = (" statCode(" + str(response.status_code) + ")") if str(response.status_code) != "200" else ""
+    logging.debug("IP(" + client_ip + ") method(" + request.method + ") path(" + request.path + ")" + stat)
 
 
 @error(404)
